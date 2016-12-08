b6c071692c9ced05d0d9b4855c148027ebb769fd
neko nekon <ponponsippo@gmail.com>
2016-12-08 13:06:21 +0900

    方位指示修正

--- js/conectrl.js
+++ js/conectrl.js
@@ -5,11 +5,14 @@
     window.addEventListener("load",function(event){
         cam = document.querySelector('#basecam');
         cone = document.querySelector('#cone');
+        directcone = document.querySelector('#directcone');
         lookloop();
     });
     function lookloop(){
         window.requestAnimationFrame(lookloop);
         var camRotation = cam.getAttribute("rotation");
+        var srad = Math.atan2(camRotation.y,camRotation.x)*180/Math.PI;
         cone.setAttribute('rotation',{x:-camRotation.x-90,y:-camRotation.y,z:-camRotation.z});
+        directcone.setAttribute('rotation',{x:0,y:0,z:srad-180});
     }
 })();
